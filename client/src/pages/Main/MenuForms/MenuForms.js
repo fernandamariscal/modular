3bda17ba6e28256b8c5a608a03213274b1d40a09
LujaMX <lujaimito@gmail.com>
2024-07-16 18:39:09 -0600

    Se realiza el cambio de base de datos a MongoDB, asi como la configuracion del backend. Ya se realiza el registro de los formularios.

---
+++ client/src/pages/Main/MenuForms/MenuForms.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './MenuForms.css'; // Importa tu archivo de estilos
+import SaveForm from './SaveForm';
+import RetireForm from './RetireForm';
+import GoalsForm from './GoalsForm';
+
+const MenuForms = ({ userId }) => { // Recibe userId como prop
+    const [selectedForm, setSelectedForm] = useState(null); // Estado para controlar qué formulario mostrar
+
+    // Función para seleccionar y mostrar el formulario adecuado
+    const selectForm = (formType) => {
+        setSelectedForm(formType);
+    };
+
+    const handleCloseForm = () => {
+        setSelectedForm(null); // Resetear el formulario seleccionado al cerrar
+    };
+
+    return (
+        <div className="menu-form">
+            <h2>¿En qué puede ayudarte FinanTec?</h2>
+            <div className="menu-options">
+                <div className="saving-box green" onClick={() => selectForm('save')}>
+                    <div className="icon">💰</div>
+                    <div>Generar Ahorro</div>
+                </div>
+                <div className="saving-box blue" onClick={() => selectForm('retirement')}>
+                    <div className="icon">🏦</div>
+                    <div>Plan de Jubilación</div>
+                </div>
+                <div className="saving-box pink" onClick={() => selectForm('goal')}>
+                    <div className="icon">🎯</div>
+                    <div>Metas Largo/Corto Plazo</div>
+                </div>
+            </div>
+
+            {/* Mostrar SaveForm si selectedForm es 'save' */}
+            {selectedForm === 'save' && <SaveForm userId={userId} onClose={handleCloseForm} />}
+            {selectedForm === 'retirement' && <RetireForm userId={userId} onClose={handleCloseForm} />}
+            {selectedForm === 'goal' && <GoalsForm userId={userId} onClose={handleCloseForm} />}
+        </div>
+    );
+}
+
+export default MenuForms;
