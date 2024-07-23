5cd49c6449b57a2691b5156a62dc26cb2df20933
fernandamariscal <maria.mariscal5008@alumnos.udg.mx>
2024-07-23 00:08:09 -0600

    Prueba para subir archivos al la branch

---
+++ client/src/pages/Subs/Jubilacion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Jubilacion = () => {
+    return (
+        <>
+            <div className="jubilacion-container">
+                <h1 className="jubilacion-title">Jubilacion</h1>
+                {/* Aquí puedes añadir el contenido específico del componente Plan de Jubilación */}
+                <div className="jubilacion-content">
+                    {/* Contenido adicional */}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Jubilacion;
