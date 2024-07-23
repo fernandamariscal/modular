3ce8723a79840e83fd0f1f3cdf95aef4a74bb271
fernandamariscal <maria.mariscal5008@alumnos.udg.mx>
2024-07-23 00:08:09 -0600

    Prueba para subir archivos al la branch

---
+++ client/src/pages/Subs/Metas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Metas = () => {
+    return (
+        <>
+            <div className="metas-container">
+                <h1 className="metas-title">Metas</h1>
+                {/* Aquí puedes añadir el contenido específico del componente Plan de Jubilación */}
+                <div className="metas-content">
+                    {/* Contenido adicional */}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Metas;
