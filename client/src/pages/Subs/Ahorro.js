ee4c5f00202cf56a096f4428da50edddcd76a969
fernandamariscal <maria.mariscal5008@alumnos.udg.mx>
2024-07-23 00:08:09 -0600

    Prueba para subir archivos al la branch

---
+++ client/src/pages/Subs/Ahorro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Ahorro = () => {
+    return (
+        <>
+            <div className="ahorro-container">
+                <h1 className="ahorro-title">Ahorro</h1>
+                {/* Aquí puedes añadir el contenido específico del componente Plan de Jubilación */}
+                <div className="ahorro-content">
+                    {/* Contenido adicional */}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Ahorro;
