9a35b2427c0e4e2a553e239bda275ee4d8c22698
LujaMX <lujaimito@gmail.com>
2024-07-30 16:23:27 -0600

    Se realiza la conexion del historial a la BD, ya muestra los registros correctamente

--- client/src/pages/Subs/Historial.js
+++ client/src/pages/Subs/Historial.js
@@ -1,148 +1,196 @@
-import React, { useState } from 'react';
-import './Historial.css';
+// Historial.js
+import React, { useState, useEffect } from "react";
+import "./Historial.css";
 
-const Historial = () => {
-    const [filterVisible, setFilterVisible] = useState(false);
-    const [selectedFilter, setSelectedFilter] = useState('');
-    const [filterValue, setFilterValue] = useState('');
-    const [filteredData, setFilteredData] = useState([]);
-    const [activeTab, setActiveTab] = useState('gastos'); // Estado para la pestaña activa
+const Historial = ({ userId }) => {
+  const [filterVisible, setFilterVisible] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [activeTab, setActiveTab] = useState("gastos");
+  const [data, setData] = useState([]); // Estado para los datos obtenidos del backend
 
-    const data = [
-        { date: '2024-07-01 12:00', name: 'Compra 1', type: 'Comida', cost: '50', category: 'gastos' },
-        { date: '2024-07-02 14:30', name: 'Compra 2', type: 'Ropa', cost: '100', category: 'gastos' },
-        { date: '2024-07-03 16:45', name: 'Compra 3', type: 'Entretenimiento', cost: '75', category: 'gastos' },
-        { date: '2024-07-01 09:00', type: 'Electricidad', frequency: 'Mensual', cost: '120', category: 'pagos' },
-        { date: '2024-07-02 11:30', type: 'Agua', frequency: 'Bimestral', cost: '80', category: 'pagos' },
-        { date: '2024-07-03 14:00', type: 'Internet', frequency: 'Anual', cost: '300', category: 'pagos' },
-        // Agrega más datos según sea necesario
-    ];
-
-    const handleFilter = () => {
-        const filtered = data.filter(item => {
-            // Verifica que `item.name` y otros campos sean definidos antes de aplicar `toLowerCase()`
-            const nameMatch = selectedFilter !== 'Nombre' || (item.name && item.name.toLowerCase().includes(filterValue.toLowerCase()));
-            const typeMatch = selectedFilter !== 'Tipo' || (item.type && item.type.toLowerCase().includes(filterValue.toLowerCase()));
-            const dateMatch = selectedFilter !== 'Fecha' || (item.date && item.date.includes(filterValue));
-            const frequencyMatch = selectedFilter !== 'Frecuencia' || (item.frequency && item.frequency.toLowerCase().includes(filterValue.toLowerCase()));
-    
-            return nameMatch && typeMatch && dateMatch && frequencyMatch && item.category === activeTab;
-        });
-    
-        setFilteredData(filtered);
-    };
-    
-
-    const toggleFilterVisibility = () => {
-        setFilterVisible(!filterVisible);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/expenses/showExpenses?userId=${userId}`
+        );
+        const expenses = await response.json();
+        const responseServices = await fetch(
+          `http://localhost:5000/api/expenses/showServices?userId=${userId}`
+        );
+        const services = await responseServices.json();
+        const combinedData = [
+          ...expenses.map((expense) => ({ ...expense, category: "gastos" })),
+          ...services.map((service) => ({ ...service, category: "pagos" })),
+        ];
+        setData(combinedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    const handleTabChange = (tab) => {
-        setActiveTab(tab);
-        setFilteredData(data.filter(item => item.category === tab));
-        if (tab === 'pagos') {
-            setSelectedFilter(''); // Desactiva el filtro al cambiar de pestaña
-        }
-    };
+    fetchData();
+  }, [userId]);
 
-    const calculateTotal = () => {
-        return (filteredData.length > 0 ? filteredData : data.filter(item => item.category === activeTab))
-            .reduce((acc, item) => acc + parseFloat(item.cost), 0)
-            .toFixed(2);
-    };
+  const handleFilter = () => {
+    const filtered = data.filter((item) => {
+      const nameMatch =
+        selectedFilter !== "Nombre" ||
+        (item.name &&
+          item.name.toLowerCase().includes(filterValue.toLowerCase()));
+      const typeMatch =
+        selectedFilter !== "Tipo" ||
+        (item.type &&
+          item.type.toLowerCase().includes(filterValue.toLowerCase()));
+      const dateMatch =
+        selectedFilter !== "Fecha" ||
+        (item.date && item.date.includes(filterValue));
+      const frequencyMatch =
+        selectedFilter !== "Frecuencia" ||
+        (item.frequency &&
+          item.frequency.toLowerCase().includes(filterValue.toLowerCase()));
 
+      return (
+        nameMatch &&
+        typeMatch &&
+        dateMatch &&
+        frequencyMatch &&
+        item.category === activeTab
+      );
+    });
+
+    setFilteredData(filtered);
+  };
+
+  const toggleFilterVisibility = () => {
+    setFilterVisible(!filterVisible);
+  };
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+    setFilteredData(data.filter((item) => item.category === tab));
+    if (tab === "pagos") {
+      setSelectedFilter("");
+    }
+  };
+
+  const calculateTotal = () => {
     return (
-        <div className="historial-container">
-            <div className="tabs">
-                <button 
-                    className={`tab-button ${activeTab === 'gastos' ? 'active' : ''}`} 
-                    onClick={() => handleTabChange('gastos')}
-                >
-                    Historial Gastos
-                </button>
-                <button 
-                    className={`tab-button ${activeTab === 'pagos' ? 'active' : ''}`} 
-                    onClick={() => handleTabChange('pagos')}
-                >
-                    Historial Pagos Servicio
-                </button>
-                <button className="filter-btn" onClick={toggleFilterVisibility}>
-                    <i className="bi bi-sliders"></i>
-                </button>
-            </div>
-            {filterVisible && (
-                <div className="filter-options">
-                    {activeTab === 'gastos' && !selectedFilter && (
-                        <>
-                            <button onClick={() => setSelectedFilter('Nombre')}>Filtrar por Nombre</button>
-                            <button onClick={() => setSelectedFilter('Tipo')}>Filtrar por Tipo</button>
-                            <button onClick={() => setSelectedFilter('Fecha')}>Filtrar por Fecha</button>
-                        </>
-                    )}
-                    {activeTab === 'pagos' && !selectedFilter && (
-                        <>
-                            <button onClick={() => setSelectedFilter('Tipo')}>Filtrar por Tipo</button>
-                            <button onClick={() => setSelectedFilter('Frecuencia')}>Filtrar por Frecuencia</button>
-                        </>
-                    )}
-                    {selectedFilter && (
-                        <>
-                            <input
-                                type="text"
-                                placeholder={`Ingrese ${selectedFilter}`}
-                                value={filterValue}
-                                onChange={(e) => setFilterValue(e.target.value)}
-                            />
-                            <button onClick={handleFilter}>Aplicar Filtro</button>
-                            <button onClick={() => setSelectedFilter('')}>Cancelar</button>
-                        </>
-                    )}
-                </div>
-            )}
-            <table className="historial-table">
-                <thead>
-                    <tr>
-                        <th>Fecha/Hora</th>
-                        {activeTab === 'pagos' ? (
-                            <>
-                                <th>Tipo</th>
-                                <th>Frecuencia</th>
-                            </>
-                        ) : (
-                            <>
-                                <th>Nombre</th>
-                                <th>Tipo</th>
-                            </>
-                        )}
-                        <th>Costo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {(filteredData.length > 0 ? filteredData : data.filter(item => item.category === activeTab)).map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.date}</td>
-                            {activeTab === 'pagos' ? (
-                                <>
-                                    <td>{item.type}</td>
-                                    <td>{item.frequency}</td>
-                                </>
-                            ) : (
-                                <>
-                                    <td>{item.name}</td>
-                                    <td>{item.type}</td>
-                                </>
-                            )}
-                            <td>{item.cost}</td>
-                        </tr>
-                    ))}
-                    <tr className="total-row">
-                        <td colSpan={activeTab === 'pagos' ? '4' : '3'}>Total</td>
-                        <td>${calculateTotal()}</td>
-                    </tr>
-                </tbody>
-            </table>
+      filteredData.length > 0
+        ? filteredData
+        : data.filter((item) => item.category === activeTab)
+    )
+      .reduce((acc, item) => acc + parseFloat(item.cost), 0)
+      .toFixed(2);
+  };
+
+  return (
+    <div className="historial-container">
+      <div className="tabs">
+        <button
+          className={`tab-button ${activeTab === "gastos" ? "active" : ""}`}
+          onClick={() => handleTabChange("gastos")}
+        >
+          Historial Gastos
+        </button>
+        <button
+          className={`tab-button ${activeTab === "pagos" ? "active" : ""}`}
+          onClick={() => handleTabChange("pagos")}
+        >
+          Historial Pagos Servicio
+        </button>
+        <button className="filter-btn" onClick={toggleFilterVisibility}>
+          <i className="bi bi-sliders"></i>
+        </button>
+      </div>
+      {filterVisible && (
+        <div className="filter-options">
+          {activeTab === "gastos" && !selectedFilter && (
+            <>
+              <button onClick={() => setSelectedFilter("Nombre")}>
+                Filtrar por Nombre
+              </button>
+              <button onClick={() => setSelectedFilter("Tipo")}>
+                Filtrar por Tipo
+              </button>
+              <button onClick={() => setSelectedFilter("Fecha")}>
+                Filtrar por Fecha
+              </button>
+            </>
+          )}
+          {activeTab === "pagos" && !selectedFilter && (
+            <>
+              <button onClick={() => setSelectedFilter("Tipo")}>
+                Filtrar por Tipo
+              </button>
+              <button onClick={() => setSelectedFilter("Frecuencia")}>
+                Filtrar por Frecuencia
+              </button>
+            </>
+          )}
+          {selectedFilter && (
+            <>
+              <input
+                type="text"
+                placeholder={`Ingrese ${selectedFilter}`}
+                value={filterValue}
+                onChange={(e) => setFilterValue(e.target.value)}
+              />
+              <button onClick={handleFilter}>Aplicar Filtro</button>
+              <button onClick={() => setSelectedFilter("")}>Cancelar</button>
+            </>
+          )}
         </div>
-    );
+      )}
+      <table className="historial-table">
+        <thead>
+          <tr>
+            <th>Fecha/Hora</th>
+            {activeTab === "pagos" ? (
+              <>
+                <th>Tipo</th>
+                <th>Frecuencia</th>
+              </>
+            ) : (
+              <>
+                <th>Nombre</th>
+                <th>Tipo</th>
+              </>
+            )}
+            <th>Costo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(filteredData.length > 0
+            ? filteredData
+            : data.filter((item) => item.category === activeTab)
+          ).map((item, index) => (
+            <tr key={index}>
+              <td>{new Date(item.createdAt).toLocaleString()}</td>
+              {activeTab === "pagos" ? (
+                <>
+                  <td>{item.type}</td>
+                  <td>{item.frequency}</td>
+                </>
+              ) : (
+                <>
+                  <td>{item.name}</td>
+                  <td>{item.type}</td>
+                </>
+              )}
+              <td>{item.cost}</td>
+            </tr>
+          ))}
+          <tr className="total-row">
+            <td colSpan={activeTab === "pagos" ? "4" : "3"}>Total</td>
+            <td>${calculateTotal()}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Historial;
